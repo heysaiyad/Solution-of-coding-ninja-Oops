592fcb565c604b9b80048cf3b02dd0b03deb5769
amansah01 <aman2181.be21@chitkara.edu.in>
2022-04-10 15:00:13 +0530

    Sum of three number

---
+++ Basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int c;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    int sum = a + b + c;
+    cout<<sum;
+    return 0;
+}
