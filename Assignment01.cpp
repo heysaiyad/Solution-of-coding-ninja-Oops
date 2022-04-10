a645ff248dada52e418aadcd21d38d55792000cd
Md Saiyad Ali <md2455.be21@chitkara.edu.in>
2022-04-10 15:47:36 +0530

    modify and adding comment.

--- Assignment01.cpp
+++ Assignment01.cpp
@@ -1,16 +1,23 @@
+// Find post increment and post increament in given number
 #include<iostream>
 using namespace std;
 int main(){
     int x = 10;
     int y;
 
-    y = x++;
-    cout<<y<<endl;
-    cout<<x<<endl;
+    //Value of x will not be increased  before assignment.
 
+
+    y = x++;
+    cout<<"Value of x++ is :"<<y<<endl;
+
+    //After expression value of x is increased
+    cout<<"Value of x is "<<x<<endl;
+    
+    //value of x will be increased before assignment.
     y= ++x;
 
-    cout<<y<<endl;
-    cout<<x<<endl;
+    cout<<"Value of ++x is "<<y<<endl;
+    
     return 0;
 }
